3eb9c6adabf565256cca15f14204ba33f49b7b23
watFiree <hellocharlesp@gmail.com>
2022-03-13 16:09:59 +0100

    user class example

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "../classes/User.h";
 
 using namespace std;
 
 int main() {
+  User user = new User();
+
+  user.login('nazwa', 'haslo')
+
   cout << "Hello world!" << endl;
   return 0;
 };
